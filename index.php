a52c6a0fa33425768545dc463702de78647da13e
Heron Yang <v6510young88@gmail.com>
2014-10-11 12:12:04 +0800

    update for data table

---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="交大運動吃早餐 - NCTU Runway">
+    <meta name="author" content="heron">
+    <!--<link rel="icon" href="../../favicon.ico">-->
+
+    <title>NCTU Runway</title>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="material/css/ripples.min.css" rel="stylesheet">
+    <link href="material/css/material-wfont.min.css" rel="stylesheet">
+
+    <link href="custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="assets/img/headline.png" alt="NCTU Runway" width="131" height="30"/></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="https://www.facebook.com/nctu.runway/">Facebook</a></li>
+            <li><a href="mailto:">聯絡我們</a></li>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>起床跑步吃早餐了！</h1>
+        <p>這裡寫鼓勵的話，最好還有相片</p>
+        <p><a href="#intro" class="btn btn-danger btn-lg" role="button">活動辦法</a></p>
+      </div>
+
+
+      <div class="page-header">
+        <h1>排行榜</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th># 排名</th>
+                <th>工號</th>
+                <th>姓名</th>
+                <th>累積點數</th>
+              </tr>
+            </thead>
+            <tbody id="rank-tbody">
+              <tr>
+                <td>1</td>
+                <td>3001212</td>
+                <td>周杰倫</td>
+                <td>100</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>3001210</td>
+                <td>五月天</td>
+                <td>85</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>3001211</td>
+                <td>想不到</td>
+                <td>80</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div id="intro" class="page-header">
+        <h1>活動辦法</h1>
+      </div>
+
+      <div class="well">
+          <p>一、<b>參加資格</b>：交大學生、教職員、校友</p>
+          <p>二、<b>報名辦法</b>：每周二、四，12:20~13:00，在基礎科學教學研究大樓(科三)152教室登錄指紋， 報名一次就可享有全年資格<p>
+          <p>三、<b>活動內容</b>：一天當中任何時間，在一小時內跑環校一圈， 並按壓三台指紋機(不限順序)，就可在運動後兩天早上領一份早餐。ex:星期一跑，星期三領</p>
+          <p>四、<b>領取方法</b>：符合領取早餐資格者，可於學生第二餐廳SEVEN櫃檯處按壓指紋機 經店員確認後即可領取一份49元組合餐</p>
+          <p>五、<b>領取時間</b>：每天 6:00~11:00</p>
+          <p><i>主辦單位保有變更辦法之權力，活動辦法如有變更，以主辦單位公告為準</i></p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <script src="material/js/ripples.min.js"></script>
+    <script src="material/js/material.min.js"></script>
+
+</body>
+
+</html>
